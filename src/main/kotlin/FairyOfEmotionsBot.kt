4a26a1f2dcd1fcd128abbe7591c3e7a7352b3e47
Ultimatereo <mansursult9@gmail.com>
2023-01-30 17:14:57 +0300

    0.1.4 Changed logic of FOEBot and added error handling

--- src/main/kotlin/FairyOfEmotionsBot.kt
+++ src/main/kotlin/FairyOfEmotionsBot.kt
@@ -2,7 +2,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
 
-
 object FairyOfEmotionsBot : TelegramLongPollingBot() {
     class EmailValidator {
         companion object {
@@ -15,17 +14,17 @@ object FairyOfEmotionsBot : TelegramLongPollingBot() {
     }
 
     private var token = ""
-    private var dialogId = 0
-    private var emotions = listOf<String>()
-    private var currentIndex = 0
-    private var currentRate = -1
+    private var dialogMode = mutableMapOf<Long, Int>() //0
+    private var emotions = mutableMapOf<Long, List<String>>()
+    private var currentIndex = mutableMapOf<Long, Int>() //0
     private val chatIdAndSheetsId = mutableMapOf<Long, String>()
     private var isMapUpdated = false
+
     override fun getBotToken(): String {
         if (token.isEmpty()) {
             token = ProjectProperties.mainProperties.getProperty("BOT_TOKEN")
             if (token.isEmpty()) {
-                System.err.println("Bot token not found in main.properties resources!")
+                System.err.println("Bot token wasn't found in main.properties!")
             }
         }
         return token
@@ -37,114 +36,157 @@ object FairyOfEmotionsBot : TelegramLongPollingBot() {
 
     override fun onUpdateReceived(update: Update?) {
         if (!isMapUpdated) {
-            SheetsManager.getChatIdAndSheetsId(chatIdAndSheetsId)
-            isMapUpdated = true
+            try {
+                SheetsManager.getChatIdAndSheetsId(chatIdAndSheetsId)
+                isMapUpdated = true
+            } catch (e: Exception) {
+                System.err.println("Updating chat ids and sheets ids failed!")
+                e.printStackTrace()
+                return
+            }
+            for (element in chatIdAndSheetsId) {
+                dialogMode[element.key] = 0
+                currentIndex[element.key] = 0
+            }
         }
         if (update == null || update.message == null || update.message.chatId == null) {
             return
         }
         val chatId = update.message.chatId
-        if (update.hasMessage()) {
-            if (update.message.text == null) {
-                update.message.text = ""
-            }
-            println(update.message.text)
-            if (!chatIdAndSheetsId.containsKey(chatId) && !update.message.text.endsWith("start")) {
-                createMessage(chatId, Strings.START)
-            } else {
-                try {
-                    if (update.message.text == "/help") {
-                        createMessage(chatId, Strings.HELP_MESSAGE)
-                    } else if (update.message.text == "/start") {
-                        if (chatIdAndSheetsId.containsKey(chatId)) {
-                            createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
-                        } else {
-                            val sheetsId = SheetsManager.createSpreadsheetFairy()
-                            SheetsManager.addClient(sheetsId, chatId.toString())
-                            chatIdAndSheetsId[chatId] = sheetsId
-                            createMessage(chatId, Strings.START_MESSAGE)
-                            dialogId = 1
-                        }
-                    } else if (update.message.text == "/tables") {
-                        createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
-                    } else if (update.message.text == "/cancel") {
-                        if (dialogId == 1) {
-                            createMessage(chatId, Strings.END_REG)
-                        } else {
-                            createMessage(chatId, Strings.CANCELLATION_SUCCESS)
-                        }
-                        dialogId = 0
-                    } else if (update.message.text == "/add_emotion") {
-                        createMessage(chatId, Strings.WRITE_EMOTION)
-                        dialogId = 2
-                    } else if (update.message.text == "/add_rate") {
-                        if (dialogId >= 3) {
-                            createMessage(chatId, Strings.RATE_IN_PROCCESS)
-                        } else {
-                            emotions = SheetsManager.getAllEmotions(getSheetsId(chatId))
-                            dialogId = 3
-                            currentIndex = 0
-                            createMessage(chatId, Strings.RATE_EMOTIONS)
-                            createMessage(chatId, Strings.rateEmotion(emotions[currentIndex]))
-                        }
-                    } else if (update.message.text == "/get_emotions") {
-                        createMessage(chatId, Strings.writeAllEmotions(getSheetsId(chatId)))
-                    } else if (update.message.text == "/link_email") {
-                        createMessage(chatId, Strings.LINK_EMAIL)
-                        dialogId = 1
-                    } else if (dialogId != 0) {
-                        when (dialogId) {
-                            1 -> {
-                                val email = update.message.text
-                                if (EmailValidator.isEmailValid(email)) {
-                                    try {
-                                        SheetsManager.givePermissionToSpreadsheet(
-                                            getSheetsId(chatId), email
-                                        )
-                                        createMessage(chatId, Strings.accessIsGivenTo(email))
-                                        createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
-                                        dialogId = 0
-                                    } catch (e: Exception) {
-                                        createMessage(chatId, Strings.EMAIL_WRONG)
-                                    }
-                                } else {
-                                    createMessage(chatId, Strings.EMAIL_WRONG)
-                                }
-                            }
-                            2 -> {
-                                val emotionsForPrinting: List<String> =
-                                    update.message.text.filterNot { it.isWhitespace() }.split(",")
-                                SheetsManager.addEmotions(emotionsForPrinting, getSheetsId(chatId))
-                                dialogId = 0
-                                createMessage(chatId, Strings.EMOTION_ADD_SUCCESS)
-                            }
-                            3 -> {
-                                var isCorrect = false
-                                try {
-                                    currentRate = update.message.text.toInt()
-                                    if ((10 >= currentRate) && (currentRate >= 1)) {
-                                        isCorrect = true
-                                    }
-                                } catch (e: Exception) {
-                                    isCorrect = false
-                                }
-                                if (isCorrect) {
-                                    SheetsManager.addRate(emotions[currentIndex++], currentRate, getSheetsId(chatId))
-                                    if (currentIndex == emotions.size) {
-                                        dialogId = 0
-                                        createMessage(chatId, Strings.RATE_END)
-                                    }
-                                } else {
-                                    createMessage(chatId, Strings.RATE_RETRY)
-                                }
-                                createMessage(chatId, Strings.rateEmotion(emotions[currentIndex]))
-                            }
-                        }
-                    } else {
-                        createMessage(chatId, Strings.UNKNOWN_MESSAGE)
+
+        if (!update.hasMessage()) return
+
+        if (update.message.text == null) {
+            update.message.text = ""
+        }
+
+        System.err.println(update.message.text)
+
+        if (!chatIdAndSheetsId.containsKey(chatId) && update.message.text != "/start") {
+            createMessage(chatId, Strings.START)
+            return
+        }
+        when (update.message.text) {
+            "/help" -> createMessage(chatId, Strings.HELP_MESSAGE)
+            "/start" -> {
+                if (chatIdAndSheetsId.containsKey(chatId)) {
+                    createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
+                } else {
+                    val sheetsId = SheetsManager.createSpreadsheetFairy()
+                    try {
+                        SheetsManager.addClient(sheetsId, chatId.toString())
+                    } catch (e: Exception) {
+                        System.err.println("Something wrong happened when adding client to data base")
+                        e.printStackTrace()
                     }
+                    chatIdAndSheetsId[chatId] = sheetsId
+                    createMessage(chatId, Strings.START_MESSAGE)
+                    dialogMode[chatId] = 1
+                }
+            }
+            "/tables" -> createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
+            "/cancel" -> {
+                if (dialogMode[chatId] == 1) {
+                    createMessage(chatId, Strings.END_REG)
+                } else {
+                    createMessage(chatId, Strings.CANCELLATION_SUCCESS)
+                }
+                dialogMode[chatId] = 0
+            }
+            "/add_emotion" -> {
+                createMessage(chatId, Strings.WRITE_EMOTION)
+                dialogMode[chatId] = 2
+            }
+            "/add_rate" -> {
+                if (dialogMode[chatId] == 3) {
+                    createMessage(chatId, Strings.RATE_IN_PROCCESS)
+                } else {
+                    try {
+                        emotions[chatId] = SheetsManager.getAllEmotions(getSheetsId(chatId))
+                    } catch (e: Exception) {
+                        System.err.println("Something wrong happened when getting all emotions")
+                        e.printStackTrace()
+                    }
+                    dialogMode[chatId] = 3
+                    currentIndex[chatId] = 0
+                    createMessage(chatId, Strings.RATE_EMOTIONS)
+                    createMessage(chatId, Strings.rateEmotion(emotions[chatId]!![currentIndex[chatId]!!]))
+                }
+            }
+            "/get_emotions" -> {
+                try {
+                    createMessage(chatId, Strings.writeAllEmotions(getSheetsId(chatId)))
                 } catch (e: Exception) {
-                    System.err.println(e.stackTrace)
+                    System.err.println("Something went wrong when getting emotions")
+                    e.printStackTrace()
+                }
+            }
+            "/link_email" -> {
+                createMessage(chatId, Strings.LINK_EMAIL)
+                dialogMode[chatId] = 1
+            }
+            else -> {
+                when (dialogMode[chatId]) {
+                    1 -> {
+                        val email = update.message.text
+                        if (EmailValidator.isEmailValid(email)) {
+                            try {
+                                SheetsManager.givePermissionToSpreadsheet(
+                                    getSheetsId(chatId), email
+                                )
+                            } catch (e: Exception) {
+                                createMessage(chatId, Strings.EMAIL_WRONG)
+                            }
+                            createMessage(chatId, Strings.accessIsGivenTo(email))
+                            createMessage(chatId, Strings.createTablesMessage(getSheetsId(chatId)))
+                            dialogMode[chatId] = 0
+                        } else {
+                            createMessage(chatId, Strings.EMAIL_WRONG)
+                        }
+                    }
+                    2 -> {
+                        val emotionsForPrinting: List<String> =
+                            update.message.text.filterNot { it.isWhitespace() }.split(",")
+                        try {
+                            SheetsManager.addEmotions(emotionsForPrinting, getSheetsId(chatId))
+                        } catch (e: Exception) {
+                            System.err.println(
+                                "Something wen wrong when adding emotions " +
+                                        "for user with chatId $chatId"
+                            )
+                            e.printStackTrace()
+                        }
+                        dialogMode[chatId] = 0
+                        createMessage(chatId, Strings.EMOTION_ADD_SUCCESS)
+                    }
+                    3 -> {
+                        var isCorrect = false
+                        var currentRate = 0
+                        try {
+                            currentRate = update.message.text.toInt()
+                            if ((10 >= currentRate) && (currentRate >= 1)) {
+                                isCorrect = true
+                            }
+                        } catch (e: Exception) {
+                            isCorrect = false
+                        }
+                        if (isCorrect) {
+                            SheetsManager.addRate(
+                                emotions[chatId]!![currentIndex[chatId]!!],
+                                currentRate,
+                                getSheetsId(chatId)
+                            )
+                            currentIndex[chatId] = currentIndex[chatId]!! + 1
+                            if (currentIndex[chatId] == emotions.size) {
+                                dialogMode[chatId] = 0
+                                createMessage(chatId, Strings.RATE_END)
+                            }
+                        } else {
+                            createMessage(chatId, Strings.RATE_RETRY)
+                        }
+                        createMessage(chatId, Strings.rateEmotion(emotions[chatId]!![currentIndex[chatId]!!]))
+                    }
+                    else -> createMessage(chatId, Strings.UNKNOWN_MESSAGE)
                 }
             }
         }
@@ -153,9 +195,14 @@ object FairyOfEmotionsBot : TelegramLongPollingBot() {
     private fun getSheetsId(chatId: Long) = chatIdAndSheetsId[chatId]!!
 
     private fun createMessage(chatId: Long, text: String) {
-        val sendMessage = SendMessage()
-        sendMessage.chatId = chatId.toString()
-        sendMessage.text = text
-        execute(sendMessage)
+        try {
+            val sendMessage = SendMessage()
+            sendMessage.chatId = chatId.toString()
+            sendMessage.text = text
+            execute(sendMessage)
+        } catch (e: Exception) {
+            System.err.println("Something wrong happened when trying to create a message")
+            e.printStackTrace()
+        }
     }
 }
